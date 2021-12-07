ba4019a9797cb40f601c7e01910632798bbe8ada
Caleb <farmingtoncardinals24@gmail.com>
2021-12-07 17:18:53 -0600

    Profile page update and slight change to project creation page

--- Models/SeedData.cs
+++ Models/SeedData.cs
@@ -28,7 +28,7 @@ namespace gis_portfolio.Models
                         date = DateTime.Parse("2021-9-11"),               
                         author = "Connor Escajeda",
                         long_desc = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-                        photo = "/Assests/sword.jpg"
+                        
                     },
 
                     new Post
