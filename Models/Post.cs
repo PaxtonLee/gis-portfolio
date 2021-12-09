3f3066d85a3decec9310da1211acc003ed45e616
Caleb <farmingtoncardinals24@gmail.com>
2021-12-08 18:28:47 -0600

    Revert "Merge branch 'main' into Paxton/PostCreationPage"
    This reverts commit 70c83c340664cbef9a909f579cf51f92b0e7a9b1.

--- Models/Post.cs
+++ Models/Post.cs
@@ -1,5 +1,4 @@
-﻿
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -20,9 +19,6 @@ namespace gis_portfolio.Models
         [Display(Name = "Author")]
         public string author { get; set; }
         public string long_desc { get; set; }
-        public string photo { get; set; }
-        
-
 
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -8,17 +8,10 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.AspNetCore.Authentication;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authentication.OpenIdConnect;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.Identity.Web;
-using Microsoft.Identity.Web.UI;
-using Microsoft.AspNetCore.Mvc.Authorization;
-using Microsoft.AspNetCore.Http;
 
 namespace gis_portfolio
 {
@@ -34,45 +27,13 @@ namespace gis_portfolio
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
-            /*
-             * Init Microsoft Auth services
-            */
-            services.Configure<CookiePolicyOptions>(options =>
-            {
-                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-                options.CheckConsentNeeded = context => true;
-                options.MinimumSameSitePolicy = SameSiteMode.Unspecified;
-                // Handling SameSite cookie according to https://docs.microsoft.com/en-us/aspnet/core/security/samesite?view=aspnetcore-3.1
-                options.HandleSameSiteCookieCompatibility();
-            });
-
-            // Sign-in users with the Microsoft identity platform
-            services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
-            .AddMicrosoftIdentityWebApp(options => Configuration.Bind("AzureAd", options));
-
-            services.AddControllersWithViews(options =>
-            {
-                var policy = new AuthorizationPolicyBuilder()
-                    .RequireAuthenticatedUser()
-                    .Build();
-                options.Filters.Add(new AuthorizeFilter(policy));
-            }).AddMicrosoftIdentityUI();
-
-            services.AddRazorPages();
-            /*
-             * End Init Microsoft Auth
-             */
-
             services.AddDbContext<gis_portfolioContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
             services.AddDatabaseDeveloperPageExceptionFilter();
-
-            // Unsure if adding default identity will cause issues with the Microsoft Auth process. Remove below if issues are thrown
-            //services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-            //    .AddEntityFrameworkStores<gis_portfolioContext>();
-            //services.AddRazorPages();
+            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddEntityFrameworkStores<gis_portfolioContext>();
+            services.AddRazorPages();
 
             services.AddDbContext<gis_portfolioContext>(options =>
                     options.UseSqlServer(Configuration.GetConnectionString("gis_portfolioContext")));
@@ -95,7 +56,6 @@ namespace gis_portfolio
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-            app.UseCookiePolicy();
 
             app.UseRouting();
 
@@ -104,9 +64,6 @@ namespace gis_portfolio
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
                 endpoints.MapRazorPages();
             });
         }
